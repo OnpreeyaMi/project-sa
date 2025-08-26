205be4f3c084cf1895d8a52d49969438dd618e46
MutitaSingkeaw <mutitasingkeaw@gmail.com>
2025-08-26 14:57:48 +0700

    frontendEdit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,54 @@
 import React, { useState } from "react";
 // import AdminSidebar from "./component/layout/admin/AdminSidebar";
 // import Background from "./component/background";
-// import EmpSidebar from "./component/layout/employee/empSidebar";
-import CustomerSidebar from "./component/layout/customer/CusSidebar";
+import EmpSidebar from "./component/layout/Sidebar/EmpSidebar";
+import StatusUpdate from "./Feature/LaundryProcess/pages/StatusUpdate";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import HomePage from "./Roles/Employee/HomePage"; // สร้างใหม่ด้านล่าง
+import TransportQueuePage from "./Feature/Queue/page/TransportQueuePage";
+import OrderDetail from "./Feature/LaundryProcess/pages/OrderDetail"; // หน้ารายละเอียดออเดอร์ + อัพเดทสถานะ
+
+import CustomerSidebar from "./component/layout/Sidebar/CusSidebar"; //หน้าลูกค้า
+import StatusPage from "./Feature/LaundryProcess/pages/StatusPage";
+
 
 const App: React.FC = () => {
-  const [activeTab, setActiveTab] = useState<string>("");
-
   return (
-    <>
-      {/* <Background activeTab={activeTab} setActiveTab={setActiveTab} /> */}
-      {/* <AdminSidebar></AdminSidebar> */}
-      {/* <EmpSidebar></EmpSidebar> */}
-      <CustomerSidebar></CustomerSidebar>
+    <BrowserRouter>
+
+          <Routes>
+            <Route path="/" element={<EmpSidebar />}/>
+            <Route path="/Home" element={<HomePage />} />
+            <Route path="/statusupdate" element={<StatusUpdate/>} />
+            <Route path="/TransportQueuePage" element={<TransportQueuePage/>} />
+
+            <Route path="/orders/:orderId" element={<OrderDetail />} />
+            </Routes>
+        
       
-    </>
+    </BrowserRouter>
   );
+
 };
 
 export default App;
+
+//ระบบย่อย : จัดการกระบวนการซัก actor customer
+
+// const App: React.FC = () => {
+//   return (
+//     <BrowserRouter>
+      
+//       <Routes>
+//         {/* เส้นทางอื่นๆ */}
+//         <Route path="/" element={<CustomerSidebar />}/>
+//         <Route path="/status" element={<StatusPage />} />
+//       </Routes>
+   
+//     </BrowserRouter>
+//   );
+
+// };
+
+// export default App;
+
---
+++ frontend/src/Roles/Employee/HomePage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import EmpSidebar from "../../component/layout/Sidebar/EmpSidebar";
+import { Row, Col, Card } from "antd";
+const HomePage: React.FC = () => {
+  return (
+    <EmpSidebar>
+      <h1 style={ { textAlign: "center", marginTop: "20px"}}>Employee Home Page</h1>
+
+      <Row gutter={[16, 16]} justify="center" >
+        <Col xs={24} sm={12} md={8} lg={6}>
+          <Card title="Card 1" bordered={false}>
+           <p>Card content</p> 
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} md={8} lg={6}>
+          <Card title="Card 2" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
+    </EmpSidebar>
+  );
+};
+
+export default HomePage;
