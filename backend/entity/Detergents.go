1f2c9de866b20dd3423a2a2cb19a23a73e63e38a
Onpreeya Tangon <mi@MacBook-Pro-khxng-Onpreeya.local>
2025-08-27 17:13:36 +0700

    input  *

--- backend/entity/Detergents.go
+++ backend/entity/Detergents.go
@@ -6,14 +6,13 @@ import (
 
 type Detergent struct {
 	gorm.Model
-	Name string
-	Type string
-	InStock int
-	Orders []Order `gorm:"many2many:OrderDetergents;"`
-	PurchaseDetergents []PurchaseDetergent `gorm:"foreignKey:DetergentID"`
-	UserID uint
-	User User `gorm:"foreignKey:UserID;"`
-	CategoryID uint
-
+	Name 		string
+	Type 		string
+	InStock 	int
+	Orders 		[]*Order `gorm:"many2many:OrderDetergents;"`
+	PurchaseDetergents []*PurchaseDetergent `gorm:"foreignKey:DetergentID"`
+	UserID 		uint
+	User 		*User `gorm:"foreignKey:UserID;"`
+	CategoryID 	uint
 	DetergentCategory *DetergentCategory `gorm:"foreignKey:CategoryID;"`
 }
--- backend/entity/ServiceType.go
+++ backend/entity/ServiceType.go
@@ -6,9 +6,9 @@ import (
 //ประเภทของเครื่องซัก-อบ ผูกราคา
 type ServiceType struct {
 	gorm.Model
-	Type string
-	Price float64
-	Capacity int
-	Orders []Order `gorm:"many2many:OrderServicetypes;"`
+	Type 		string
+	Price 		float64
+	Capacity 	int
+	Orders 		[]*Order `gorm:"many2many:OrderServicetypes;"`
 }
 //must add mock data
