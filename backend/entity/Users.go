c59d69c8756765845c8acb0d4d2fe37a26faac42
guspuii93 <pitsinee.jana@gmail.com>
2025-09-02 15:30:17 +0700

    yoyo

--- backend/entity/Users.go
+++ backend/entity/Users.go
@@ -11,15 +11,8 @@ type User struct {
 	Status   string
 
 	RoleID uint
-	Role *Role `gorm:"foreignKey:RoleID"`
+	Roles *Role `gorm:"foreignKey:RoleID"`
 
-	CustomerID uint
 	Customers []*Customer `gorm:"foreignKey:UserID;references:ID"`
-
-	PurchaseDetergent uint
 	PurchaseDetergents []*PurchaseDetergent `gorm:"foreignKey:UserID;references:ID"`
-	
-	
-	Employee *Employee `gorm:"foreignKey:UserID"`
-
 }
