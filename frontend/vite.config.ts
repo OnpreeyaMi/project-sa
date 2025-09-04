00f07e3739c5da08a12505310b36ae0ffd6764d8
MutitaSingkeaw <mutitasingkeaw@gmail.com>
2025-09-04 17:51:48 +0700

    แก้บัคเทววิน

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,11 @@
 
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-}) */
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/verify-slip-base64': {
@@ -27,14 +18,3 @@ export default defineConfig({
     },
   },
 })
-
-
-
-
-
-
-
-
-
-
-
