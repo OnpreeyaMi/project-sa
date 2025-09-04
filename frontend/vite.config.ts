7727d2f5e0d02db468c8b22cc910f4c95d1ef31e
MutitaSingkeaw <mutitasingkeaw@gmail.com>
2025-09-04 17:51:48 +0700

    แก้บัคเทววิน

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,11 @@
 
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-}) */
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   server: {
-    proxy: {
-      '/employees': 'http://localhost:8080',
-      '/order': 'http://localhost:8080',
-      '/order-histories': 'http://localhost:8080',
-      // หรือ proxy '/api' => 'http://localhost:8080' แล้วเปลี่ยน backend ให้มี /api prefix
-    }
-  }
 })
+
 
