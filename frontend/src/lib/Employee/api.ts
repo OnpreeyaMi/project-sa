27fe629e0602ea80b0cc6a3cba0e1826c475df60
Piyatidajeanny <yeennyx@gmail.com>
2025-09-05 03:00:26 +0700

    แก้ api 8000

--- frontend/src/lib/Employee/api.ts
+++ frontend/src/lib/Employee/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const base = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: base,
