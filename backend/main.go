7f239e324ca3f0f86dbc5a125b14dcbd1903d7bc
MutitaSingkeaw <mutitasingkeaw@gmail.com>
2025-09-11 00:16:11 +0700

    มีlogin-logout แล้ว

--- backend/main.go
+++ backend/main.go
@@ -122,8 +122,6 @@ func main() {
 
 	// ---------- Machines ----------
 	router.GET("/machines", controller.GetMachines)
-	
-
 
 	// Queue Routes
 	router.GET("/queues", controller.GetQueues) // ?type=pickup / delivery
