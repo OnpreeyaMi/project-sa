05e091974097a12aff0c7717010d40a702e3ded2
Piyatidajeanny <yeennyx@gmail.com>
2025-09-11 21:02:17 +0700

    ปรับแก้หน้าพนักงานเรียบร้อย

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "./hooks/UserContext";
 import { RequireRole } from "./routes/guards";
-
 import EmployeePage from "./pages/employee"; // index.tsx
 import LaundryCheckPage from "./pages/laundryCheck"; // index.tsx
 import TransportQueuePage from "./pages/Queue/TransportQueuePage";
@@ -11,10 +10,8 @@ import RegisterForm from "./pages/register/register";
 import EmployeeHome from "./pages/Home/EmployeeHome";
 import StatusUpdate from "./pages/LaundryProcess/StatusUpdate";
 import OrderDetail from "./pages/LaundryProcess/OrderDetail";
-
 import StockEmpPage from "./pages/stock/employee"; // src/pages/stock/employee/index.tsx
 import EmployeeProfile from "./pages/employee/EmployeeProfile";
-
 import CustomerManagement from "./pages/customer/CustomerMangement";
 import PromotionManagement from "./pages/promotion/PromotionManagement";
 import Profile from "./pages/profile/profile";
@@ -26,6 +23,7 @@ import HistoryPage from "./pages/orders/history";
 import StockAdminPage from "./pages/stock/Admin";
 import CustomerHomePage from "./pages/Home/CustomerHome";
 import LaundryHistoryPage from "./pages/laundryCheck/LaundryHistoryPage";
+import StatusPage from "./pages/LaundryProcess/StatusPage";
 
 const App: React.FC = () => {
   return (
@@ -58,6 +56,7 @@ const App: React.FC = () => {
 
         {/* Customer */}
         <Route path="/customer">
+          <Route path="status" element={<RequireRole role="customer"><StatusPage /></RequireRole>} />
           <Route path="complaint" element={<RequireRole role="customer"><CustomerComplaintPage /></RequireRole>} />
           <Route path="payment" element={<RequireRole role="customer"><Payment /></RequireRole>} />
           <Route path="orders" element={<RequireRole role="customer"><OrderPage /></RequireRole>} />
